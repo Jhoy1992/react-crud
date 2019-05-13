66e3236dafcdf9091db2ff3ac4920af49124326f
Jhonatan Ascari <desenvolvimento01@ztech.inf.br>
2019-05-13 15:52:43 -0300

    My first crud with react.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,192 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import loadingGif from './loading.gif';
 import './App.css';
+import ListItem from './ListItem.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {  
+
+  constructor() {
+
+    super();
+
+    this.state = {
+
+      newTodo: '',
+
+      editing: false,
+
+      editingIndex: null,
+
+      notification: null,
+
+      loading: true,
+      
+      todos: []
+    }
+
+    this.apiUrl = 'https://5cd97d190b004000147204cc.mockapi.io/'
+
+    this.handleChange = this.handleChange.bind(this)    
+    this.addTodo = this.addTodo.bind(this)
+    this.deleteTodo = this.deleteTodo.bind(this)
+    this.updateTodo = this.updateTodo.bind(this)        
+    this.alert =  this.alert.bind(this)
+  }
+
+  async componentDidMount() {
+    const response = await axios.get(`${this.apiUrl}/todos`)
+   
+    this.setState({
+      todos: response.data,
+      loading: false
+    })
+  }
+
+  handleChange(event) {
+
+    this.setState({
+      newTodo: event.target.value
+    })    
+  }
+
+  async addTodo(){
+
+    const response = await axios.post(`${this.apiUrl}/todos`, {
+      name: this.state.newTodo
+    })    
+
+    const todos = this.state.todos
+    todos.push(response.data)
+    this.setState({
+      todos: todos,
+
+      newTodo: ''    
+    })
+
+    this.alert('Todo added successfully.')    
+  }
+
+  async deleteTodo(index){
+    const todos = this.state.todos     
+    const todo = todos[index]
+    await axios.delete(`${this.apiUrl}/todos/${todo.id}`)
+
+    delete todos[index]
+
+    this.setState({ todos })
+    this.alert('Todo deleted successfully.')
+  }
+
+  editTodo(index){  
+    const todo = this.state.todos[index]    
+
+    this.setState({
+      editing: true,
+
+      editingIndex: index,
+
+      newTodo: todo.name
+    })  
+  }
+
+  async updateTodo() {
+    const todo = this.state.todos[this.state.editingIndex]
+    
+    this.setState({
+      loading: true
+    })          
+
+    const response = await axios.put(`${this.apiUrl}/todos/${todo.id}`, {
+      name: this.state.newTodo      
+    })
+    
+    const todos = this.state.todos
+    todos[this.state.editingIndex] = response.data
+
+    this.setState({ 
+      todos, 
+      loading: false,      
+      editing: false, 
+      editingIndex: null, 
+      newTodo: ''   
+    })
+
+    this.alert('Todo updated successfully.')      
+  }
+
+  alert(notification) {
+    this.setState({
+      notification
+    })
+
+    setTimeout(() => {
+      this.setState({notification: null})
+    }, 2000)
+  }
+
+  render() {
+    
+    return (
+
+      <div className="App">
+        <div className="container">
+
+          {
+            this.state.notification && 
+
+            <div className="alert mt-3 alert-success">            
+              {this.state.notification}                       
+            </div>
+          }
+
+          <input 
+            type="text" 
+            name="todo"
+            className="form-control mt-4 mb-2" 
+            placeholder="Add a new todo" 
+            onChange={this.handleChange}
+            value={this.state.newTodo}
+          />
+
+          <button             
+            onClick={this.state.editing ? this.updateTodo : this.addTodo}
+            className="btn-success form-control mb-4"
+            disabled={this.state.newTodo.length < 5}
+          >
+            {this.state.editing ? 'Update todo' : 'Add todo'}
+          </button>
+
+          {
+            this.state.loading &&
+            <img src={loadingGif} alt="" />
+          }          
+
+          {            
+            
+            (!this.state.editing || this.state.loading) &&
+
+            <ul className="list-group">
+              
+              {this.state.todos.map((item, index) => {                                                                                
+
+                return <ListItem                 
+
+                  key={item.id}
+                  item={item}
+                  editTodo={() => { this.editTodo(index) }}
+                  deleteTodo={() => { this.deleteTodo(index) }}
+                  
+                />
+              })}
+
+            </ul> 
+          }     
+
+        </div>
+      </div>      
+    );
+  }
 }
 
 export default App;
---
+++ src/ListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ListItem = (props) => {        
+            
+    return <li className="list-group-item">
+
+        <button className="btn btn-sm btn-info mr-4"
+        onClick={props.editTodo}
+        >U</button>
+
+        {props.item.name}
+
+        <button className="btn btn-sm btn-danger ml-4"
+        onClick={props.deleteTodo}
+        >X</button>
+    </li>   
+                   
+}      
+
+export default ListItem;
